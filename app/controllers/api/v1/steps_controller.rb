228cac77adcbf7d81b97d42064367628d2e2bf79
Mika88 <mikaspivak@gmail.com>
2019-07-23 11:21:24 -0700

    Add create action and private step_params method to StepsController

--- app/controllers/api/v1/steps_controller.rb
+++ app/controllers/api/v1/steps_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::StepsController < ApplicationController
         @steps = Step.all
         render json: @steps
     end
+
+    def create
+        @step = Step.new(step_params)
+       if @step.save
+         render json: @step
+       else
+         render json: { errors: { message: "Step failed to save"}}
+       end
+    end
+
+    private 
+
+    def step_params
+      params.require(:step).permit(:text, :goal_id)
+    end
+
 end
