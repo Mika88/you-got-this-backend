888d6b9041ec20f1a2597c0b56ae6cd294ad79fb
Mika88 <mikaspivak@gmail.com>
2019-07-28 13:23:31 -0700

    Add index and create action methods, and a private event_params method to EventsController

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::EventsController < ApplicationController
+    def index
+        @events = Event.all
+        render json: @events
+    end
+
+    def create
+        @event = Event.new(event_params)
+       if @event.save
+         render json: @event
+       else
+         render json: { errors: { message: "Event failed to save"}}
+       end
+    end
+
+    private 
+
+    def step_params
+      params.require(:event).permit(:time, :step_id)
+    end
 
 end
