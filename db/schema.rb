c2b504780b579431d0c1e814183848e40f96f0b7
Mika88 <mikaspivak@gmail.com>
2019-07-28 13:15:39 -0700

    Change 'time' column type to string in events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_223530) do
+ActiveRecord::Schema.define(version: 2019_07_28_201225) do
 
   create_table "events", force: :cascade do |t|
-    t.datetime "time"
+    t.string "time"
     t.integer "step_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
