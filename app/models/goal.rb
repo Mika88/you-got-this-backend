68743781481e6b4d97b4465f85f2f194e4e03db6
Mika88 <mikaspivak@gmail.com>
2019-10-26 10:24:34 -0700

    Add associations between users and goals. Add validations to User model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,6 @@
 class Goal < ApplicationRecord
     has_many :steps, dependent: :destroy
+    belongs_to :user
     
     validates :text, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :goals, dependent: :destroy
+
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
