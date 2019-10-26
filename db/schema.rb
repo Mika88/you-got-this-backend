d6bb2b5c38cd1a4417316c089c7cf6893659512e
Mika88 <mikaspivak@gmail.com>
2019-10-26 13:42:29 -0700

    Add a user_id column to goals table to account for goals-users association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_170705) do
+ActiveRecord::Schema.define(version: 2019_10_26_203647) do
 
   create_table "events", force: :cascade do |t|
     t.string "time"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_10_26_170705) do
     t.date "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "steps", force: :cascade do |t|
