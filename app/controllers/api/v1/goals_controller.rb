4993af03550f7aaedd9ce8d716b02df370a2be39
Mika88 <mikaspivak@gmail.com>
2019-10-26 14:47:54 -0700

    Add user_id to goal_params in GoalsController

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -21,6 +21,6 @@ class Api::V1::GoalsController < ApplicationController
     private 
 
     def goal_params
-      params.require(:goal).permit(:text, :about, :deadline)
+      params.require(:goal).permit(:text, :about, :deadline, :user_id)
     end
 end
