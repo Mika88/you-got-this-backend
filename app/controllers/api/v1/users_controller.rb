adeb5e7a693e261b7672c5d4e91828c246a80158
Mika88 <mikaspivak@gmail.com>
2019-11-03 17:18:43 -0800

    Add create action to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { errors: { message: "user failed to save"}}
+    end
+  end
 end
