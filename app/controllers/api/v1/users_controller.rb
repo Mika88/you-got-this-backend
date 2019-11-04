1b7be9ca82c91540c822b059d331c11776818e27
Mika88 <mikaspivak@gmail.com>
2019-11-03 17:24:38 -0800

    Add a user_params private method to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,4 +17,10 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id]).destroy
     render json: @user
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
